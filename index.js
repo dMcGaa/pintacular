321cb447f98806ae076fbb579fe845a1498c8cbc
dmcgaa <mcgaa.david@gmail.com>
2016-01-02 21:47:35 +0000

    basic site

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
 });
-server.listen(8080);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
