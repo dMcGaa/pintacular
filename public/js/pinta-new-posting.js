71800f2462b73790dcfe0a3a1bb0bf8056a9ba80
dmcgaa <mcgaa.david@gmail.com>
2016-02-11 19:51:37 +0000

    image preview on new posting

---
+++ public/js/pinta-new-posting.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $( "#img-url" ).change(function() {
+        var imgUrl = $("#img-url").val();
+        if(imgUrl == ""){
+            $("#img-preview").attr("src", "/images/placeholder-400x400.png");
+        }
+        else{
+            $("#img-preview").attr("src", $("#img-url").val());
+        }
+        // $('#img-preview').fadeOut(400,function(){
+        //     $(this).fadeIn(400);
+        // });
+    });
+})
